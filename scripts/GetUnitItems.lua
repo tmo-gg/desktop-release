422f4d3e2fb7190ac88ec7c1463fc8ccf3d2580c
leehoegyu <leehoegyu@gmail.com>
2025-04-27 23:10:16 +0900

    Update GetUnitItems.lua

--- scripts/GetUnitItems.lua
+++ scripts/GetUnitItems.lua
@@ -1,29 +1,12 @@
-function GetUnitItems(unit)
-    local items = {}
-    for i = 0, 5 do
-        local itemId = War3.GetUnitItem(unit, i)
-        if itemId then
-            table.insert(items, itemId)
-        end
-    end
-    return items
-end
-
 Callbacks.Bind("Run", function()
     if War3.WorldInitialized then
         if (GetAsyncKeyState(33) % 2 ~= 0) then  -- VK_PRIOR(Page Up)
             local unit = War3.GetSelectedUnit()
-            if unit then
-                local items = GetUnitItems(unit)
-                
-                local message = "선택한 유닛의 아이템 목록:"
-                for i, itemId in ipairs(items) do
-                    message = message .. "\n" .. i .. ". " .. itemId
+            if unit then                
+                for i = 0, 5 do
+                    local itemId = War3.GetUnitItem(unit, i)
+                    print(itemId)
                 end
-                
-                print(message)
-            else
-                print("선택한 유닛이 없습니다.")
             end
         end
     end
